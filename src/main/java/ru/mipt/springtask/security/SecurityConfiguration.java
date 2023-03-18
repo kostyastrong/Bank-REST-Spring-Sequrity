f214136032f3189de5f235d4ddb4ec2a1bf883d3
kostyastrong <kostyakostya872@gmail.com>
2023-03-18 19:34:40 +0300

    admin/user added, ddl error occurs

--- src/main/java/ru/mipt/springtask/security/SecurityConfiguration.java
+++ src/main/java/ru/mipt/springtask/security/SecurityConfiguration.java
@@ -1,21 +1,27 @@
 package ru.mipt.springtask.security;
 
+import antlr.BaseAST;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
 
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("student")
-//                .password("123")
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password("123")
-//                .roles("ADMIN");
-//    }
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("student")
+                .password(passwordEncoder.encode("123"))
+                .roles("STUDENT")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN");
+    }
 }
